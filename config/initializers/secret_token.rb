f3f90e22036a78134a3e4ee5767b6bf6577acf13
Nathan Benes <nathan.benes@gmail.com>
2013-02-22 21:52:07 -0700

    just ran rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gemtd::Application.config.secret_token = '2daf2f9b817c68982aa2b5276942cd7d5d659654cec6c27981be23e888024c43f7d2ad5650b33dfa86c2df55bf059f2d577f739d378218b05c428b9f7214a795'
